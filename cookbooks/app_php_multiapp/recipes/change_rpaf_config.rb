76c1ba4fb029b4733aefb5423a0d6c551c686e06
Jesse "tux" Estomago <jesse@scorp.io>
2013-09-26 11:55:32 +0200

    Revised

--- cookbooks/app_php_multiapp/recipes/change_rpaf_config.rb
+++ cookbooks/app_php_multiapp/recipes/change_rpaf_config.rb
@@ -6,10 +6,10 @@ class Chef::Recipe
   include RightScale::App::Helper
 end
 
-pool_names(node[:lb][:pools]).each do |pool_name|
-  app_php_multiapp "Update rpaf config" do
-    machine_tag "loadbalancer:#{pool_name}=lb"
-    ip_tag "server:#{node[:app][:backend_ip_type]}_ip_0"
-    action :update_rpaf
-  end
+pool_name="default"
+
+app_php_multiapp "Update rpaf config" do
+  machine_tag "loadbalancer:#{pool_name}=lb"
+  ip_tag "server:#{node[:app][:backend_ip_type]}_ip_0"
+  action :update_rpaf
 end
