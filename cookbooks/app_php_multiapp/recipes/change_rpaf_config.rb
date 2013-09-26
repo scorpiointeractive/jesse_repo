2379247595f5a8550b596e4f4db0156f3899209c
Jesse "tux" Estomago <jesse@scorp.io>
2013-09-26 11:20:54 +0200

    Files support for change rpaf

---
+++ cookbooks/app_php_multiapp/recipes/change_rpaf_config.rb
@@ -0,0 +1,9 @@
+Cookbook Name:: app_php_multiapp
+
+rightscale_marker
+
+class Chef::Recipe
+  include RightScale::App::Helper
+end
+
+
---
+++ cookbooks/app_php_multiapp/resources/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: app_php_multiapp
+#
+
+
+actions :update_rpaf
+
+# Sets regex for identifying machine tags in servers on which remote_recieps can
+# be run
+attribute :machine_tag, :kind_of => String, :regex => /^([^:]+):(.+)=.+/
+
+# Sets the IP interface on which the server listens. Example: public_ip_0, private_ip_0
+attribute :ip_tag, :kind_of => String, :regex => /server:(.+)/
+
+# Server collection
+attribute :collection, :kind_of => String, :default => "mycollection"
+
+# Defines a default action
+def initialize(*args)
+  super
+  @action = :update
+end
